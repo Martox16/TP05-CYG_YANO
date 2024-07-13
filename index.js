48b9839e04d01e547af29f3e65d0ab64fb178571
Martin <martucyg@gmail.com>
2024-07-12 23:59:51 -0300

    aa

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 import express from "express"
-import EventController from "./src/controllers/event-controller.js";
-import UserController from "./src/controllers/user-controller.js";
-import ProvinciasController from "./src/controllers/provincias-controller.js";
-import LocationController from "./src/controllers/location-controller.js"
-import CategoryController from "./src/controllers/category-constroller.js";
-import eventLocationCotroller from "./src/controllers/event-location.js"
+import Event from "./src/controllers/event-controller.js";
+import User from "./src/controllers/user-controller.js";
+import Provincias from "./src/controllers/provincias-controller.js";
+import Location from "./src/controllers/location-controller.js"
+import Category from "./src/controllers/category-constroller.js";
+import eventLocation from "./src/controllers/event-location.js"
 const app = express(); 
 app.use(express.json()); 
 const port = 3408;
-
-app.use("/event", EventController);
-app.use("/user", UserController);
-app.use("/province", ProvinciasController);
-app.use("/location", LocationController)
-app.use("/event-category", CategoryController)
-app.use("/event-location", eventLocationCotroller)
+//1
+app.use("/event", Event);
+//2
+app.use("/user", User);
+//3
+app.use("/province", Provincias);
+//4
+app.use("/location", Location);
+//5
+app.use("/event-category", Category);
+//6
+app.use("/event-location", eventLocation);
 
 app.listen(port, () =>{
     console.log("mandalo al servido")
